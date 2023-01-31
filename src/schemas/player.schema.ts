856669e5ec0641fb195f54206a1b968f5bf79aee
paxs <pedro.alexandre.soares@nttdata.com>
2023-01-31 23:46:25 +0000

    createPlayers and updatePlayers schemas

--- src/schemas/player.schema.ts
+++ src/schemas/player.schema.ts
@@ -5,6 +5,17 @@ export const createPlayersSchema = object({
         players: array(object({
             name: string({ required_error: 'Name is required' }),
             picture: string({ required_error: 'Picture is required' }).url('invalid url'),
+            cost: number({ required_error: 'cost is required' }).int().nonnegative(),
+            realTeam: string().min(24, 'ids should be a valid MongoDB ObjectId')
+        }))
+    })
+});
+
+export const updatePlayersSchema = object({
+    body: object({
+        round: number({required_error: 'Round number is required'}),
+        players: array(object({
+            name: string({ required_error: 'Name is required' }),
             rating: number({ required_error: 'Rating is required' }).nonnegative('ratings cant be negative'),
             acs: number({ required_error: 'acs is required' }).nonnegative('acs cant be negative'),
             kd: number({ required_error: 'kd is required' }).nonnegative('kd cant be negative'),
@@ -16,20 +27,9 @@ export const createPlayersSchema = object({
             fdpr: number({ required_error: 'fdpr is required' }).nonnegative('fdpr cant be negative'),
             hs: number({ required_error: 'hs is required' }).int(),
             roundPoints: array(number({ required_error: 'points is required' }).int().nonnegative()),
-            cost: number({ required_error: 'cost is required' }).int().nonnegative(),
-            realTeam: string().min(24, 'ids should be a valid MongoDB ObjectId')
         }))
     })
 });
-
-export const updatePlayersSchema = object({
-    body: object({
-    })
-});
-
-
-
-
 
 
 export type CreatePlayersInput = TypeOf<typeof createPlayersSchema>['body'];
